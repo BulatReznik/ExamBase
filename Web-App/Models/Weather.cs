15fb8d73dd88fe05411c965849a7410dba84fa23
BulatReznik <90419704+BulatReznik@users.noreply.github.com>
2024-05-25 21:52:26 +0400

    Поменял карточки

--- Web-App/Models/Weather.cs
+++ Web-App/Models/Weather.cs
@@ -5,9 +5,8 @@
         public string location { get; set; }
         public DateTime date { get; set; }
         public double temperature { get; set; }
-        public string name { get; set; }
-        public string windSpeed { get; set; }
-        public string humidity { get; set; }
+        public double windSpeed { get; set; }
+        public double humidity { get; set; }
         public string dateString { get; set; }
     }
 }
